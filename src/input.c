387c9b1e89f8d35d762bf352edf2bbc20362b0c8
Guy Martin <gmsoft@tuxicoman.be>
2011-11-09 17:47:29 +0000

    Remove last commented obsolete ptype_make_atomic() call in input.c.

--- src/input.c
+++ src/input.c
@@ -263,9 +263,6 @@ err:
 
 int input_register_param(struct input *i, char *name, struct ptype *value, char *default_value, char *description, unsigned int flags) {
 
-//	if (ptype_make_atomic(value) != POM_OK)
-//		return POM_ERR;
-
 	// Create the input_param structure
 
 	struct input_param *p = malloc(sizeof(struct input_param));
