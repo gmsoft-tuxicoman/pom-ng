58dfbc5368bfbfe7089adb46cdc9539bf7d75938
Guy Martin <gmsoft@tuxicoman.be>
2012-09-19 12:55:23 +0200

    Add functions to access event datas as well as iterators.

--- src/addon_event.c
+++ src/addon_event.c
@@ -20,6 +20,131 @@
 
 #include "addon.h"
 #include "addon_event.h"
+#include "addon_ptype.h"
+
+static int addon_event_data_item_iterator(lua_State *L) {
+
+	struct data_item **d = lua_touserdata(L, lua_upvalueindex(1));
+
+	if (!*d)
+		return 0;
+
+	lua_pushstring(L, (*d)->key);
+	addon_ptype_push(L, (*d)->value);
+	
+	*d = (*d)->next;
+
+	return 2;
+
+}
+
+static int addon_event_data_item_get_field(lua_State *L) {
+
+	struct addon_event *e = lua_touserdata(L, lua_upvalueindex(1));
+	const char *key = luaL_checkstring(L, 2);
+	int field_id = lua_tointeger(L, lua_upvalueindex(2));
+
+	struct data_reg *data_reg = e->evt->reg->info->data_reg;
+
+	if (field_id >= data_reg->data_count)
+		luaL_error(L, "field_id %u bigger than data_count %u", field_id, data_reg->data_count);
+
+	// Create an iterator if need
+	if (!strcmp(key, "iter")) {
+		struct data_item **d = lua_newuserdata(L, sizeof(struct data_item **));
+		*d = e->evt->data[field_id].items;
+		lua_pushcclosure(L, addon_event_data_item_iterator, 1);
+		return 1;
+	}
+
+	struct data_item *item;
+	for (item = e->evt->data[field_id].items; item && strcmp(item->key, key); item = item->next);
+	if (!item)
+		return 0;
+	
+	addon_ptype_push(L, item->value);
+
+	return 1;
+
+}
+
+static int addon_event_data_push_item(lua_State *L, int item_id) {
+	
+	// Create the item table
+	lua_newtable(L);
+
+	// Create its metatable
+	lua_newtable(L);
+
+	// Add the lookup function
+	lua_pushliteral(L, "__index");
+	lua_pushvalue(L, lua_upvalueindex(1));
+	lua_pushinteger(L, item_id);
+	lua_pushcclosure(L, addon_event_data_item_get_field, 2);
+	lua_settable(L, -3);
+
+	// Set the metatable
+	lua_setmetatable(L, -2);
+
+	return 1;
+}
+
+static int addon_event_data_iterator(lua_State *L) {
+
+	struct addon_event *e = lua_touserdata(L, lua_upvalueindex(1));
+	int id = lua_tointeger(L, lua_upvalueindex(2));
+
+	struct data_reg *data_reg = e->evt->reg->info->data_reg;
+	struct data *data = e->evt->data;
+	
+	if (id >= data_reg->data_count)
+		return 0;
+
+	lua_pushstring(L, data_reg->items[id].name);
+	if (data_reg->items[id].flags & DATA_REG_FLAG_LIST) {
+		addon_event_data_push_item(L, id);
+	} else {
+		addon_ptype_push(L, data[id].value);
+	}
+
+	id++;
+	lua_pushinteger(L, id);
+	lua_replace(L, lua_upvalueindex(2));
+
+	return 2;
+}
+
+static int addon_event_data_get_field(lua_State *L) {
+	
+	struct addon_event *e = lua_touserdata(L, lua_upvalueindex(1));
+	const char *key = luaL_checkstring(L, 2);
+
+	// Look for the field
+	struct data_reg *data_reg = e->evt->reg->info->data_reg;
+	struct data *data = e->evt->data;
+	
+	int i;
+	for (i = 0; i < data_reg->data_count; i ++) {
+		if (!strcmp(data_reg->items[i].name, key))
+			break;
+	}
+
+	if (i >= data_reg->data_count) {
+		// Not found
+		lua_pushnil(L);
+		return 1;
+	}
+
+	if (data_reg->items[i].flags & DATA_REG_FLAG_LIST) {
+		addon_event_data_push_item(L, i);
+		return 1;
+	} else {
+		addon_ptype_push(L, data[i].value);
+		return 1;
+	}
+
+	return 0;
+}
 
 static int addon_event_get_field(lua_State *L) {
 
@@ -28,6 +153,24 @@ static int addon_event_get_field(lua_State *L) {
 	const char *key = luaL_checkstring(L, 2);
 	if (!strcmp(key, "name")) {
 		lua_pushstring(L, e->evt->reg->info->name);
+	} else if (!strcmp(key, "data")) {
+		// Create an empty table
+		lua_newtable(L);
+
+		// Push the iterator function
+		lua_pushliteral(L, "iter");
+		lua_pushvalue(L, 1);
+		lua_pushinteger(L, 0);
+		lua_pushcclosure(L, addon_event_data_iterator, 2);
+		lua_settable(L, -3);
+
+		// Add a new metatable
+		lua_newtable(L);
+		lua_pushliteral(L, "__index");
+		lua_pushvalue(L, 1);
+		lua_pushcclosure(L, addon_event_data_get_field, 1);
+		lua_settable(L, -3);
+		lua_setmetatable(L, -2);
 	} else {
 		lua_pushnil(L);
 	}
