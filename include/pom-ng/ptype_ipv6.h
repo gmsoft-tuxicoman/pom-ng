2a8fc1830d52ae7e9fd52890f295c448d1ff6b57
Guy Martin <gmsoft@tuxicoman.be>
2012-11-06 21:08:39 +0100

    Add missing ptype_ipv6.h.

---
+++ include/pom-ng/ptype_ipv6.h
@@ -0,0 +1,44 @@
+/*
+ *  This file is part of pom-ng.
+ *  Copyright (C) 2010 Guy Martin <gmsoft@tuxicoman.be>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef __POM_NG_PTYPE_IPV6_H__
+#define __POM_NG_PTYPE_IPV6_H__
+
+#include <pom-ng/ptype.h>
+
+#include <netinet/in.h>
+
+struct ptype_ipv6_val {
+	struct in6_addr addr;
+	unsigned char mask;
+};
+
+
+/// x is the struct ptype,
+#define PTYPE_IPV6_GETADDR(x) \
+	((struct ptype_ipv6_val*) x->value)->addr
+
+/// x is the struct ptype, y the value
+#define PTYPE_IPV6_SETADDR(x, y) {			\
+	struct ptype_ipv6_val *v = (x)->value;		\
+	memcpy(&v->addr, &y, sizeof(struct in6_addr));	\
+}
+
+#endif
