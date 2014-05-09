94edb5ea081ddd5e3a64cc3c3a698b1d81fa7779
Guy Martin <gmsoft@tuxicoman.be>
2014-05-09 15:52:31 +0200

    Update copyright years.

--- src/modules/output/output_tap.h
+++ src/modules/output/output_tap.h
@@ -1,6 +1,6 @@
 /*
  *  This tap is part of pom-ng.
- *  Copyright (C) 2012-2013 Guy Martin <gmsoft@tuxicoman.be>
+ *  Copyright (C) 2012-2014 Guy Martin <gmsoft@tuxicoman.be>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
