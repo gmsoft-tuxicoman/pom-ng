9a2c528874636b57fef75d722bcfb8624255cf8f
D3v <crazysnob@live.it>
2025-12-22 13:02:07 +0100

    addon: replace luaL_register() with luaL_setfuncs()
    luaL_register() is Lua 5.1-era/removed in newer Lua; use luaL_setfuncs() for Lua 5.2+ compatibility in addon_event, addon_output and addon_plugin.

--- src/addon_plugin.c
+++ src/addon_plugin.c
@@ -340,7 +340,7 @@ int addon_plugin_lua_register(lua_State *L) {
 	luaL_newmetatable(L, ADDON_PLUGIN_METATABLE);
 
 	// Register the functions
-	luaL_register(L, NULL, m);
+	luaL_setfuncs(L, m, 0);
 
 	return POM_OK;
 }
